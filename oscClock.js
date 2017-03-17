0e319ea9ca64ba4968114291789dc2ad57092f15
idcavili <Ian.Cavilia@gmail.com>
2017-03-17 18:05:26 -0400

    Update and rename LICENSE.md to oscClock.js

---
+++ oscClock.js
@@ -0,0 +1,25 @@
+var phase = 0;
+var twoPi = 6.28;
+var rate;
+module.exports = function(){
+  return{
+    setFreq:function(freq){
+      this.rate = twoPi * freq / system.fs;
+     }
+     getPhase:function(){
+      return this.phase;
+     }
+     setPhase:function(phase){
+      this.phase = Math.floor(phase / this.rate) * this.rate:
+     }
+     getNextValue:function(){
+      if(this.phase == this.twoPi){
+        this.phase = 0;
+       }
+       else{
+        this.phase += this.rate;
+       }
+      return this.phase;
+     }
+    }
+   }
