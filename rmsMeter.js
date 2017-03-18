b99b2c07b366394b1aecddd833d6541f8d16211d
idcavili <Ian.Cavilia@gmail.com>
2017-03-18 14:47:10 -0400

    Update rmsMeter.js

--- rmsMeter.js
+++ rmsMeter.js
@@ -1,23 +1,23 @@
 var size;
 var sample = 0;
 var sum = 0;
+var rms = 0;
 module.exports = function(){
   return{
     setWindowSize:function(size){
       this.size = size * system.fs;
-    }
+    },
     calculate:function(value){
       this.sum += Math.sqr(value);
       if(this.sample == (this.size - 1)){
-        var rms = Math.sqrt(this.sum - this.size);
+        this.rms = Math.sqrt(this.sum / this.size);
         this.sum = 0;
         this.sample = 0;
-        return rms;
       }
       else{
         this.sample++;
-        return -1;
       }
+      return this.rms;
    }
   }
 }
