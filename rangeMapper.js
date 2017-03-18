77ccd66b2fc60b77c7958b76ee5230dd7d67067b
idcavili <Ian.Cavilia@gmail.com>
2017-03-18 14:48:39 -0400

    Update rangeMapper.js

--- rangeMapper.js
+++ rangeMapper.js
@@ -5,7 +5,7 @@ module.exports = function(){
     setRange:function(inMin, inMax, outMin, outMax){
       this.slope = (outMax - outMin) / (inMax - inMin);
       this.intercept = outMax - inMax;
-     }
+     },
      map:function(value){
       return value * this.slope + this.intercept;
      }
