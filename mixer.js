239bea9624c708faa83625024636981d38908820
idcavili <Ian.Cavilia@gmail.com>
2017-03-20 15:13:30 -0400

    Update mixer.js

--- mixer.js
+++ mixer.js
@@ -98,11 +98,11 @@ function onInit(){
 function changeInputCount(path, value){
   if(value > lastInputCount){
     for(i=lastInputCount + 1;i<=value;i++){
-      addInput(i);
+      addCh("input", i);
     }
   }else{
     for(i=lastInputCount;i>value;i--){
-      removeInput(i);
+      removeCh("input", i);
     }
   }
   lastInputCount = value;
@@ -110,11 +110,11 @@ function changeInputCount(path, value){
 function changeGroupCount(path, value){
   if(value > lastGroupCount){
     for(i=lastGroupCount + 1;i<=value;i++){
-      addGroup(i);
+      addCh("group", i);
     }
   }else{
     for(i=lastGroupCount;i>value;i--){
-      removeGroup(i);
+      removeCh("group", i);
     }
   }
   lastGroupCount = value;
@@ -122,11 +122,11 @@ function changeGroupCount(path, value){
 function changeAuxCount(path, value){
   if(value > lastAuxCount){
     for(i=lastAuxCount + 1;i<=value;i++){
-      addAux(i);
+      addCh("aux", i);
     }
   }else{
     for(i=lastAuxCount;i>value;i--){
-      removeAux(i);
+      removeCh("aux", i);
     }
   }
   lastAuxCount = value;
@@ -134,83 +134,123 @@ function changeAuxCount(path, value){
 function changeMatrixCount(path, value){
   if(value > lastMatrixCount){
     for(i=lastMatrixCount + 1;i<=value;i++){
-      addMatrix(i);
+      addCh("matrix", i);
     }
   }else{
     for(i=lastMatrixCount;i>value;i--){
-      removeMatrix(i);
+      removeCh("matrix", i);
     }
   }
   lastMatrixCount = value;
 }
-function addInput(n){
-  addInput(["input", "n", "in", "l"]);
-  addInput(["input", "n", "in", "r"]);
-  addOutput(["input", "n", "insert", "send", "l"]);
-  addOutput(["input", "n", "insert", "send", "r"]);
-  addInput(["input", "n", "insert", "return","l"]);
-  addInput(["input", "n", "insert", "return","r"]);
+function addCh(type, n){
+  var path;
+  path[0] = type;
+  var idx = (type == "main")?1:2;
+  if(type != "main"){
+    path[1] = n;
+  }
+  if(type == "input"){
+    path[idx] = "in"
+  }else{
+    path[idx] = "subin"
+  }
+  path[idx + 1] = "l"
+  addInput(path);
+  path[idx + 1] = "r"
+  addInput(path);
+  path[idx] = "insert";
+  path[idx + 1] = "send";
+  path[idx + 2] = "l";
+  addOutput(path);
+  path[idx + 2] = "r";
+  addOutput(path);
+  path[idx + 1] = "return";
+  path[idx + 2] = "l";
+  addInput(path);
+  path[idx + 2] = "r";
+  addInput(path);
+  if(type != input){
+    path[idx] = "out"
+    path[idx + 1] = "l"
+    addOutput(path);
+    path[idx + 1] = "r"
+    addOutput(path);
+  }
+  path[idx] = "name";
+  addParamater(path);
+  setParameterType(path, 2);
+  setParameterValue(path, "Input " + n);
   
-  addParamater(["input", "n", "name"]);
-  setParameterType(["input", "n", "name"], 2);
-  setParameterValue(["input", "n", "name"], "Input " + n);
-  
-  addParameter(["input", "n", "vol", "val"]);
-  setParameterType(["input", "n", "vol", "val"], 0);
-  setParameterRange(["input", "n", "vol", "val"], -inf, 20, 0.1, "dB");
-  setParameterCallback(["input", "n", "vol", "val"], new function(path, value){
-    inputs.n.vol.target = Math.pow(10, value / 20);
-    inputs.n.vol.rate = (inputs.n.vol.target - inputs.n.vol.current) / (getParameter(["input", "n", "vol", "time"]) * system.fs);
+  path[idx] = "vol";
+  path[idx + 1] = "val";
+  addParameter(path);
+  setParameterType(path, 0);
+  setParameterRange(path, -inf, 20, 0.1, "dB");
+  setParameterCallback(path, new function(path, value){
+    channel.path[0].path[1].vol.target = Math.pow(10, value / 20);
+    channel.path[0].path[1].vol.rate = (inputs.n.vol.target - inputs.n.vol.current) / (getParameter(["input", "n", "vol", "time"]) * system.fs);
   });
-  setParameterValue(["input", "n", "vol", "val"], 0, true);
+  setParameterValue(path, 0, true);
   
-  addParameter(["input", "n", "vol", "imm"]);
-  setParameterType(["input", "n", "vol", "imm"], 0);
-  setParameterRange(["input", "n", "vol", "imm"], -inf, 20, 0.1, "dB");
-  setParameterCallback(["input", "n", "vol", "imm"], new function(path, value){
-    inputs.n.vol.current = Math.pow(10, value / 20);
-    inputs.n.vol.target = inputs.n.vol.target;
+  path[idx + 1] = "imm";
+  addParameter(path);
+  setParameterType(path, 0);
+  setParameterRange(path, -inf, 20, 0.1, "dB");
+  setParameterCallback(path, new function(path, value){
+    channel.path[0].path[1].vol.current = Math.pow(10, value / 20);
+    channel.path[0].path[1].vol.target = inputs.n.vol.target;
   });
-  setParameterValue(["input", "n", "vol", "imm"], 0, true);
-  removeFromState(["input", "n", "vol", "imm"], true);
+  setParameterValue(path, 0, true);
+  removeFromState(path, true);
   
-  addParameter(["input", "n", "vol", "time"]);
-  setParameterType(["input", "n", "vol", "time"], 0);
-  setParameterRange(["input", "n", "vol", "time"], 0, 20, 0.01, "s");
-  setParameterCallback(["input", "n", "vol", "time"], new function(path, value){
-    inputs.n.vol.rate = (inputs.n.vol.target - inputs.n.vol.current) / (getParameter(["input", "n", "vol", "time"]) * system.fs);
+  path[idx + 1] = "time"
+  addParameter(path);
+  setParameterType(path, 0);
+  setParameterRange(path, 0, 20, 0.01, "s");
+  setParameterCallback(path, new function(path, value){
+    channel.path[0].path[1].rate = (inputs.n.vol.target - inputs.n.vol.current) / (getParameter(["input", "n", "vol", "time"]) * system.fs);
   });
-  setParameterValue(["input", "n", "vol", "time"], 0, true);
+  setParameterValue(path, 0, true);
   
-  addParameter(["input", "n", "pan"]);
-  setParameterType(["input", "n", "pan"], 0);
-  setParameterRange(["input", "n", "pan"], 0, 1, 0.01, "");
-  setParameterValue(["input", "n", "pan"], 0.5, true);
+  path.length = idx + 1;
+  path[idx] = "pan";
+  addParameter(path);
+  setParameterType(path, 0);
+  setParameterRange(path, 0, 1, 0.01, "");
+  setParameterValue(path, 0.5, true);
   
-  addParameter(["input", "n", "width"]);
-  setParameterType(["input", "n", "width"], 0);
-  setParameterRange(["input", "n", "width"], 0, 1, 0.01, "");
-  setParameterValue(["input", "n", "width"], 0.5, true);
+  path[idx] = "width";
+  addParameter(path);
+  setParameterType(path, 0);
+  setParameterRange(path, 0, 1, 0.01, "");
+  setParameterValue(path, 0.5, true);
   
-  addParameter(["input", "n", "mute"]);
-  setParameterType(["input", "n", "mute"], 1);
-  setParameterStates(["input", "n", "mute"], [0, 1], ["Unmuted", "Muted"]);
-  setParameterValue(["input", "n", "mute"], 0, true);
+  path[idx] = "mute";
+  addParameter(path);
+  setParameterType(path, 1);
+  setParameterStates(path, [0, 1], ["Unmuted", "Muted"]);
+  setParameterValue(path, 0, true);
   
-  addParameter(["input", "n", "solo"]);
-  setParameterType(["input", "n", "solo"], 1);
-  setParameterStates(["input", "n", "solo"], [0, 1], ["Off", "On"]);
-  setParameterValue(["input", "n", "solo"], 0, true);
+  if(type != main){
+    path[idx] = "solo";
+    addParameter(path);
+    setParameterType(path, 1);
+    setParameterStates(path, [0, 1], ["Off", "On"]);
+    setParameterValue(path, 0, true);
   
-  addParameter(["input", "n", "safe"]);
-  setParameterType(["input", "n", "safe"], 1);
-  setParameterStates(["input", "n", "safe"], [0, 1], ["Off", "On"]);
-  setParameterValue(["input", "n", "safe"], 0, true);
+    path[idx] = "safe";
+    addParameter(path);
+    setParameterType(path, 1);
+    setParameterStates(path, [0, 1], ["Off", "On"]);
+    setParameterValue(path, 0, true);
+  }
   
-  addParameter(["input", "n", "insert"]);
-  setParameterType(["input", "n", "insert"], 1);
-  setParameterStates(["input", "n", "insert"], [0, 1], ["Off", "On"]);
-  setParameterValue(["input", "n", "insert"], 0, true);
+  path[idx] = "insert";
+  addParameter(path);
+  setParameterType(path, 1);
+  setParameterStates(path, [0, 1], ["Off", "On"]);
+  setParameterValue(path, 0, true);
   
   addParameter(["input", "n", "mono"]);
   setParameterType(["input", "n", "mono"], 1);
