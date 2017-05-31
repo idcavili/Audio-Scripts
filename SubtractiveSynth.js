8f6bb7d60e3b8112c33e6d5d1bdb955ed9ee34b6
idcavili <Ian.Cavilia@gmail.com>
2017-05-30 21:22:44 -0400

    Update SubtractiveSynth.js

--- SubtractiveSynth.js
+++ SubtractiveSynth.js
@@ -1,6 +1,9 @@
 var OscClock = require("OscClock");
 var sustain = 0;
 var sostenuto = 0;
+var highestVoice;
+var lowestVoice;
+var voiceAges[];
 var keys[];
 var voices[];
 
@@ -26,12 +29,34 @@ function changeVoice(voice, pitch, vel){
     ampEnv[voice].setStage(0);
   }
 }
+function triggerVoice(voice, pitch, vel){
+  voices[voice].vel = vel;
+  voices[voice].key = pitch;
+  voices[voice].targetPitch = microTune(ptich);
+  if(getParameter("portamento", "sw") == 1){
+    voices[voice].rate = (voices[voice].targetPitch - voices[voice].currentPitch) / portTime;
+  }
+  else{
+    voices[voice].currentPitch = voices[voice].targetPitch;
+  }
+  pitchEnv[voice].setTime(0, getParameter("pitch", "time", "attack") + vel * getParameter("vel", "pitchAttack"));
+  filterEnv[voice].setTime(0, getParameter("filter", "env", "time", "attack") + vel * getParameter("vel", "filterAttack"));
+  ampEnv[voice].setTime(0, getParameter("amp", "attack") + vel * getParameter("vel", "ampAttack"));
+  ampEnv[voice].setTime(0, getParameter("amp", "decay") + vel * getParameter("vel", "ampDecay"));
+  ampEnv[voice].setLevel(0, getParameter("amp", "sustain") + vel * getParameter("vel", "ampSustain"));
+  pitchEnv[voice].setStage(0);
+  filterEnv[voice].setStage(0);
+  ampEnv[voice].setStage(0);
+}
 
 function onSample(){
   var events = getEvents("in");
   for(i=0;i<events.length;i++){
     switch(events[i].type){
       case 0x80: //note off
+        if(keys[events[i].data[0]] == 0){
+          continue;
+        }
         keys[events[i].data[0]] = 0;
         if(getParameter("voices") == 1){  //monophonic
           if(sustain == 1 || sostenuto == 1){
@@ -72,7 +97,10 @@ function onSample(){
             continue;
           }
           for(j=0;j<getParameter("voices");j++){
-            if(voices[j].vel > 0 && voices[j].key == events[i].data[0] && voices[j].sostenuto == 0){
+            if(voices[j].key == events[i].data[0]){
+              if(voices[j].sostenuto == 1 && sostenuto == 1){
+                break;
+              }
               killVoice(j);
               updateHighLow();
               removeVoiceAge(j);
@@ -80,4 +108,28 @@ function onSample(){
               break;
             }
           }
+        }
+       case 0x90: //note on
+         if(keys[events[i].data[0]] > 0){
+           continue;
+         }
+        var vel = events[i].data[1] << 7 & hrv;
+        hrv = 0;
+        keys[events[i].data[0]] = vel;
+        if(getParameter("voices") == 1){ //monophonic
+          var pitch = -1;
+          switch(getParameter("voiceMode")){
+            case HIGHEST:
+              if(events[i].data[0] > voices[0].key){
+                pitch = events[i].data[0];
+                break;
+            case LOWEST:
+              if(events[i].data[0] < voices[0].key){
+                pitch = events[i].data[0];
+                break;
+            case NEWEST:
+              pitch = events[i].data[0];
+           }
+          
+                
             
